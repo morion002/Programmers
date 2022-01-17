02b0afa6beb1b359ef007f437c56de67dd683558
morion002 <87214089+morion002@users.noreply.github.com>
2022-01-17 10:33:38 +0900

    Update 신규 아이디 추천.py

--- Level1/신규 아이디 추천.py
+++ Level1/신규 아이디 추천.py
@@ -1,15 +1,19 @@
 def solution(new_id):
     answer = ''
+    
     # 1단계
     new_id = new_id.lower()
+    
     # 2단계
     available = ['-', '_', '.']
     for i in range(len(new_id)):
         if(new_id[i].isalpha() or new_id[i].isdigit() or new_id[i] in available):
             answer += new_id[i]
+            
     # 3단계      
     while (answer.find("..") != -1):
         answer = answer.replace("..", ".")
+        
     # 4단계
     id_list = list(answer)
     if(id_list[0] == "."):
@@ -17,15 +21,18 @@ def solution(new_id):
     if(len(id_list) != 0):
         if(id_list[-1] == "."):
             del(id_list[-1])
+            
     # 5단계
     if(len(id_list) == 0):
         id_list.append('a')
+        
     # 6단계
     if(len(id_list) > 15):
         del(id_list[15:])
     if(len(id_list) != 0):
         if(id_list[-1] == "."):
             del(id_list[-1])
+            
     # 7단계
     if(len(id_list) < 3):
         while(len(id_list) < 3):
