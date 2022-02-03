d13b01e37a4bfcc7a7705d7075fcb83de3909b1b
morion002 <87214089+morion002@users.noreply.github.com>
2022-02-03 12:13:52 +0900

    Update K번째수.py

--- Level1/K번째수.py
+++ Level1/K번째수.py
@@ -1,7 +1,7 @@
 def solution(array, commands):
     answer = []
-    for q in range(len(commands)):
-        i, j, k = commands[q][0], commands[q][1], commands[q][2]
+    for command in commands:
+        i, j, k = command
         temp = []
         for p in range(i-1, j):
             temp.append(array[p])
